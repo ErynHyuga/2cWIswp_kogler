e98d7c7185d05b0bcb2e1ae3bd4ef76001a9003b
ErynHyuga <you@example.com>
2023-03-08 09:33:55 +0100

    eya

---
+++ 01Vars/02_js_strings/s.js
@@ -0,0 +1,9 @@
+let a = 17.4;
+let b = 3.9;
+c = a / b
+console.log("The result of " + a + " divided by " + b + " is " + c);
+
+let surname = prompt("Namenseingabe: Vorname");
+let lastname = prompt("Namenseingabe: Nachname");
+
+console.log("Ich heiße " + surname + " " + lastname);
---
+++ 01Vars/03_aufgaben/sc.js
@@ -0,0 +1,20 @@
+let apple = "Apple";
+let banana = "Banane";
+
+let appkg = 2.14;
+let bppkg = 3.43;
+
+apple = 0.34
+banana = 0.22
+
+let pa = 1 / apple;
+let pb = 1 / banana;
+
+console.log("Der Preis von Äpfel ist " + appkg + " € pro kilo ")
+console.log("Der Preis von Bananen ist " + bppkg + " € pro kilo"); 
+
+console.log("8 Äpfel kosten " + (8 * apple) * appkg + "€");
+console.log("17 Bananen kosten " + (17 * banana) * bppkg + "€");
+
+console.log("eine Tonne Äpfel kosten " + 1000 * appkg + "€");
+console.log("eine Tonne Bananen kosten " + 1000 * bppkg + "€");
