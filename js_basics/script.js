d13387b0c526209d4cdfd350fe0b0217d841ce2f
ErynHyuga <you@example.com>
2023-03-08 08:56:43 +0100

    01 JS var und operators done

---
+++ js_basics/script.js
@@ -0,0 +1,42 @@
+console.log("Hello");
+console.log("yupppie");
+//Var
+let a = 7;
+let b = 8;
+let c = 10;
+
+
+console.log(a + b + c);
+
+console.log((a + b) / c);
+
+console.log(c - a + b);
+
+console.log(a + b + c * 3);
+
+console.log((a + b + c) / 4);
+
+a = 3
+b = a + 3
+c = c - 3
+
+console.log(a + b + c);
+
+console.log((a + b) / c);
+
+console.log(c - a + b);
+
+console.log(a + b + c * 3);
+
+console.log((a + b + c) / 4);
+/*
+console.log("a hat den wert " + a);
+
+
+console.log("a hat den wert " + a);
+
+let c = a + b;
+console.log("a + b = " + c);
+
+c = b - a;
+console.log("b - a = " + c);*/
