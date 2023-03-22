1403cd1db07a4c473c2108c2ccb2cd5116cc85a2
ErynHyuga <you@example.com>
2023-03-22 08:56:35 +0100

    Aufgabe 1 und 2 von 05Schleifen abgeschlossen

---
+++ 01Vars/05_if/curcumabrot.js
@@ -0,0 +1,33 @@
+// erstelle eine Variable number
+
+
+// Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+
+
+// Aufgabe 2
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+
+let na = 42;
+
+if (na<10) {
+    console.log("Mini");
+}else if (20<na<50) {
+    console.log("Medium");
+} else if (50<na) {
+    console.log("Large");
+}
+
+let nb = 1;
+
+if (nb<7) {
+    console.log("Mini");
+}else if (7<nb<90) {
+    console.log("Medium");
+} else if (90<nb) {
+    console.log("Large");
+}
