ad8217fcf1b7003ff97a525e64c28b96717b25d0
ErynHyuga <you@example.com>
2023-03-22 08:47:04 +0100

    04 Schleifen wurde abgeschlossen

---
+++ 01Vars/04_schleifen/brot.js
@@ -0,0 +1,34 @@
+console.log("test");
+
+console.log("Jaoist");
+
+console.log(" zahlen 7 - 45");
+for (let count = 7; count < 46; count++) {
+    console.log(count);
+}
+
+console.log(" zählt von 9 zu 99");
+for (let count = 9; count < 100; count++) {
+    console.log(count);
+}
+
+console.log(" zählt von 10 zu 0 herab");
+for (let count = 10; count > -1; count--) {
+    console.log(count);
+}
+
+console.log("zählt von 1234 zu 1207 herab");
+for (let count = 1234; count > 1206; count--) {
+    console.log(count);
+}
+
+
+for (let count = 0; count < 10; count++) {
+    console.log(count);
+}
+
+console.log("Eric Kogler");
+
+for (let count = 10; count > 0; count--) {
+    console.log(count);
+}
