f90caab9853310b34ee040528f850bbe57bc3d23
ErynHyuga <you@example.com>
2023-03-29 15:43:00 +0200

    SwitchStatement abgeschlossen

---
+++ 01Vars/08_Switch/donerkebab.js
@@ -0,0 +1,21 @@
+
+let rN = Math.random() * 5 + 5;
+let ra = Math.floor(rN);
+
+console.log(ra);
+
+
+
+if (ra == 10) {
+    console.log("TEN");
+} else if (ra == 9) {
+    console.log("NINE");
+} else if (ra == 8) {
+    console.log("EIGHT");
+} else if (ra == 7) {
+    console.log("SEVEN");
+} else if (ra == 6) {
+    console.log("SIX");
+} else if (ra == 5) {
+    console.log("FIVe");
+}
