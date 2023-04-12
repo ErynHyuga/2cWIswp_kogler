523e68a6bb05befcbd2e06ae71d34a2d8d4e8a0b
ErynHyuga <you@example.com>
2023-04-12 09:35:21 +0200

    zusammenzählen auf hundert funktoniert nicht

--- 01Vars/09_aufgabezeichen/schnitzel.js
+++ 01Vars/09_aufgabezeichen/schnitzel.js
@@ -1,23 +1,35 @@
 let a = 7;
 let b = 3;
+let c = 0;
+let d = 0;
 
 
-
-for (let index = 0; index < 1; index++) {
+for (let indexa = 0; indexa < 1; indexa++) {
     console.log("X X X X");
     console.log("O O O O");
     console.log("X X X X");
     console.log("O O O O");
-    if (index % 5 == 0) {
+    if (indexa % 5 == 0) {
         console.log("");
     }
 }
 
 
-for (let index = 0; index < 1; index++) {
+for (let indexb = 1; indexb < 101; indexb++) {
+    if(indexb%1==0){
+        c = c + indexb;
+    }
+    if(indexb%2==0){
+        d = d + indexb;
+    }
+}
+
+
+for (let indexc = 0; indexc < 1; indexc++) {
     console.log("X X X X");
     console.log("X");
     console.log("X X X X");
     console.log("X");
     console.log("X X X X");
 }
+
