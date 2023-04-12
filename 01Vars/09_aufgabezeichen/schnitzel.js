5b74b1df6099e1689a5737697f470fd7f0077ef8
ErynHyuga <you@example.com>
2023-04-12 09:13:00 +0200

    odd&even finished First character of name finished

---
+++ 01Vars/09_aufgabezeichen/schnitzel.js
@@ -0,0 +1,23 @@
+let a = 7;
+let b = 3;
+
+
+
+for (let index = 0; index < 1; index++) {
+    console.log("X X X X");
+    console.log("O O O O");
+    console.log("X X X X");
+    console.log("O O O O");
+    if (index % 5 == 0) {
+        console.log("");
+    }
+}
+
+
+for (let index = 0; index < 1; index++) {
+    console.log("X X X X");
+    console.log("X");
+    console.log("X X X X");
+    console.log("X");
+    console.log("X X X X");
+}
