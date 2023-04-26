4fcebb00cd4e11a2aa51d8422f27d82b04745a13
ErynHyuga <you@example.com>
2023-04-26 11:43:05 +0200

    11_arrays has been started but not finished yet

---
+++ 01Vars/11_arrays/tutten.js
@@ -0,0 +1,19 @@
+if(prompt("Freund oder Mathe") === "M"){
+    let arr = [4, 1, 2, 3];
+    console.log(arr[1]);
+
+    arr.push(17);
+    arr.push(199);
+
+    for(let index = 0; index < arr.length; index++) {
+        console.log(arr[index]);
+    }
+
+}
+
+
+
+
+
+
+
