d364e00ec8fd25ae7a9ae099992b40aa69d0b63a
ErynHyuga <you@example.com>
2023-05-03 09:16:12 +0200

    12_input was started

--- 01Vars/11_arrays/tutten.js
+++ 01Vars/11_arrays/tutten.js
@@ -1,14 +1,30 @@
-if(prompt("Freund oder Mathe") === "M"){
+let p = prompt("Freund oder Mathe");
+if (p === "M") {
     let arr = [4, 1, 2, 3];
     console.log(arr[1]);
 
     arr.push(17);
     arr.push(199);
 
-    for(let index = 0; index < arr.length; index++) {
+    for (let index = 0; index < arr.length; index++) {
         console.log(arr[index]);
     }
+    console.log();
 
+    let ges = arr.reduce((total, num) => total + num);
+
+
+    let loeng = arr.length;
+    let middy = ges / loeng;
+
+    console.log("Gesamtsamt des arryas isch " + ges + ". " + "Der Mittelwert isch " + middy);
+
+} else if (p === "F") {
+    let ry = ["Susi ", "Paula ", "HANZ "];
+    console.log("Freundes meiner sind " + ry + ". Warte, da fehlt einer.");
+    let Felht = prompt("Wer isses?");
+    ry.push(Felht);
+    console.log("Freundes meiner sind " + ry);
 }
 
 
