9441e212f4acbc4510eb7a9fda40f8f29e420d88
ErynHyuga <you@example.com>
2023-06-07 09:28:59 +0200

    14_tictactoe muss noch weiter bearbeitet werden

--- 01Vars/14.1_TicTöcTo/tack.js
+++ 01Vars/14.1_TicTöcTo/tack.js
@@ -4,15 +4,15 @@ let feld = [
     [0, 0, 0]
 ];
 
-feld[0][0] = 1;
-feld[1][0] = 2;
-feld[2][0] = 1;
-feld[0][1] = 2;
-feld[1][1] = 1;
-feld[2][1] = 1;
-feld[0][2] = 2;
-feld[1][2] = 2;
-feld[2][2] = 1;
+feld[0][0] = a;
+feld[1][0] = b;
+feld[2][0] = c;
+feld[0][1] = d;
+feld[1][1] = e;
+feld[2][1] = r;
+feld[0][2] = f;
+feld[1][2] = g;
+feld[2][2] = h;
 printFeld();
 
 function printFeld() {
@@ -33,35 +33,3 @@ function printFeld() {
         console.log(outputRei);
     }
 }
-
-
-
-/*let feld = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-];
-
-feld[0][1] = 1;
-feld[1][1] = 2;
-printFeld();
-
-function printFeld() {
-    for (let rei = 0; rei < feld.length; rei++) {
-        let actualRei = feld[rei];
-        let outputRei = "";
-        for (let spa = 0; spa < actualRei.length; spa++) {
-            let zeicha = " ";
-
-            if (actualRei[spa] == 1) {
-                zeicha = "X";
-            } else if (actualRei[spa] == 2) {
-                zeicha = "O";
-            };
-
-            outputRei += zeicha + " | ";
-        }
-        console.log(outputRei);
-    }
-};
-*/
