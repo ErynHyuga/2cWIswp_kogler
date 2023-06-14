413e157d43d25e41fad52da7a8bc0e157a993ff7
ErynHyuga <you@example.com>
2023-06-14 09:18:49 +0200

    12_finished

--- 01Vars/14.1_TicTöcTo/tack.js
+++ 01Vars/14.1_TicTöcTo/tack.js
@@ -1,51 +1,111 @@
+function printFeld() {
+    for (let rei = 0; rei < feld.length; rei++) {
+        let actualRei = feld[rei];
+        let outputRei = "";
+        for (let spa = 0; spa < actualRei.length; spa++) {
+            let zeicha = " ";
+
+            if (actualRei[spa] === 1) {
+                zeicha = "X";
+            } else if (actualRei[spa] === 2) {
+                zeicha = "O";
+            }
+
+            outputRei += zeicha + " | ";
+        }
+        console.log(outputRei);
+    }
+}
+
+function bvoll() {
+    for (let rei = 0; rei < feld.length; rei++) {
+        for (let spa = 0; spa < feld[rei].length; spa++) {
+            if (feld[rei][spa] === 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function gwin(spieler) {
+
+    for (let rei = 0; rei < feld.length; rei++) {
+        if (
+            feld[rei][0] === spieler &&
+            feld[rei][1] === spieler &&
+            feld[rei][2] === spieler
+        ) {
+            return true;
+        }
+    }
+
+
+    for (let spa = 0; spa < feld[0].length; spa++) {
+        if (
+            feld[0][spa] === spieler &&
+            feld[1][spa] === spieler &&
+            feld[2][spa] === spieler
+        ) {
+            return true;
+        }
+    }
+
+
+    if (
+        feld[0][0] === spieler &&
+        feld[1][1] === spieler &&
+        feld[2][2] === spieler
+    ) {
+        return true;
+    }
+
+    if (
+        feld[0][2] === spieler &&
+        feld[1][1] === spieler &&
+        feld[2][0] === spieler
+    ) {
+        return true;
+    }
+
+    return false;
+}
+
+function spiel() {
+    let spieler = 1;
+
+    while (!bvoll()) {
+        console.log(`Spieler ${spieler}, gib koords`);
+        console.log("Reihe 0 bis 2");
+        let reihe = parseInt(prompt("Reihe (0-2):"));
+        console.log("Spalte 0 bis 2");
+        let spalte = parseInt(prompt("Spalte (0-2):"));
+
+        if (reihe >= 0 && reihe < 3 && spalte >= 0 && spalte < 3) {
+            if (feld[reihe][spalte] === 0) {
+                feld[reihe][spalte] = spieler;
+                console.log("funtz");
+                printFeld();
+
+                if (gwin(spieler)) {
+                    console.log(`Spieler ${spieler} hot gwonna`);
+                    return;
+                }
+                spieler = spieler === 1 ? 2 : 1;
+            } else {
+                console.log("feld voll");
+            }
+        } else {
+            console.log("funtz net");
+        }
+    }
+
+    console.log("Felder voll");
+}
 let feld = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]
-  ];
-  
-  feld[0][0] = 0;
-  feld[1][0] = 0;
-  feld[2][0] = 0;
-  feld[0][1] = 0;
-  feld[1][1] = 0;
-  feld[2][1] = 0;
-  feld[0][2] = 0;
-  feld[1][2] = 0;
-  feld[2][2] = 0;
-  
-  printFeld();
-  
-  function printFeld() {
-    for (let rei = 0; rei < feld.length; rei++) {
-      let actualRei = feld[rei];
-      let outputRei = "";
-      for (let spa = 0; spa < actualRei.length; spa++) {
-        let zeicha = " ";
-  
-        if (actualRei[spa] === 1) {
-          zeicha = "X";
-        } else if (actualRei[spa] === 2) {
-          zeicha = "O";
-        }
-  
-        outputRei += zeicha + " | ";
-      }
-      console.log(outputRei);
-    }
-  }
-  console.log("Feld.");
-console.log("Reiha 0 bis 2");
-let reihe = parseInt(prompt("Reiha (0-2):"));
-console.log("Spalta 0 bis 2");
-let spalte = parseInt(prompt("Spalta (0-2):"));
+];
 
-console.log("1 = X; 2 = O");
-let zeichen = parseInt(prompt("Zeichen"));
-if (zeichen !== 1 && zeichen !== 2) {
-  console.log("Funtz netta");
-} else {
-  feld[reihe][spalte] = zeichen;
-  console.log("Funzt");
-  printFeld();
-}
+spiel();
